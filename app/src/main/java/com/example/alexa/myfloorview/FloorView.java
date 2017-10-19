16b9118cd63345275ab4c00c62125ba68e246964
alexander_topilskii <alexander_topilskii@epam.com>
2017-10-20 00:15:04 +0300

    initial2

--- app/src/main/java/com/example/alexa/myfloorview/FloorView.java
+++ app/src/main/java/com/example/alexa/myfloorview/FloorView.java
@@ -16,11 +16,11 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class FloorView extends LinearLayout {
-    @BindView(R.id.floor_button)
-    Button topView1;
+    @BindView(R.id.floor_up_button)
+    Button upButton;
 
-    @BindView(R.id.floor_button2)
-    Button topView2;
+    @BindView(R.id.floor_down_button)
+    Button downButton;
 
     @BindView(R.id.recycler_floor)
     RecyclerView recyclerView;
@@ -42,11 +42,46 @@ public class FloorView extends LinearLayout {
         super(context, attrs, defStyleAttr, defStyleRes);
     }
 
-    void initView(){
+    void initView() {
         View view = LayoutInflater.from(getContext()).inflate(R.layout.floor_layout, this);
         ButterKnife.bind(this, view);
 
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        downButton.setVisibility(VISIBLE);
+        upButton.setVisibility(GONE);
+        final LinearLayoutManager layout = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layout);
         recyclerView.setAdapter(new FloorAdapter(15));
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                if (layout.findFirstCompletelyVisibleItemPosition() == 0) {
+                    downButton.setVisibility(VISIBLE);
+                    upButton.setVisibility(GONE);
+                }
+                if (layout.findLastCompletelyVisibleItemPosition() == 14) {
+                    downButton.setVisibility(GONE);
+                    upButton.setVisibility(VISIBLE);
+                }
+            }
+        });
+
+        downButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                recyclerView.scrollToPosition(14);
+                downButton.setVisibility(GONE);
+                upButton.setVisibility(VISIBLE);
+            }
+        });
+
+        upButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                recyclerView.scrollToPosition(0);
+                downButton.setVisibility(VISIBLE);
+                upButton.setVisibility(GONE);
+            }
+        });
     }
 }
